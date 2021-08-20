4cb30a93695104e7cc464b52f327c71f96734cfe
Kotáb Petr <518-kotabpe@users.noreply.localhost>
2021-08-20 19:08:16 +0200

    Addedd abstract classes for simple multi-types systems.

--- GameLib/Systems/Base/GameSystem.cs
+++ GameLib/Systems/Base/GameSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace GameLib
 {
     /// <summary>
@@ -16,5 +17,128 @@ namespace GameLib
         /// </summary>
         /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
         public abstract void Update(float deltaTime);
+    }
+
+    /// <summary>
+    /// Represent a game system which supports one type.
+    /// </summary>
+    public abstract class GameSystem<T> : GameSystem
+        where T : IComponent
+    {
+        protected GameSystem() 
+            : base(typeof(T)) { }
+
+        public override void Update(float deltaTime)
+        {
+            PreUpdate(deltaTime);
+            foreach (var entity in Entities)
+            {
+                T component = entity.Get<T>();
+
+                UpdateItem(deltaTime, component);
+            }
+        }
+
+        /// <summary>
+        /// Occur before update.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PreUpdate(float deltaTime) { }
+
+        /// <summary>
+        /// Occur after update.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PostUpdate(float deltaTime) { }
+
+        /// <summary>
+        /// Update item.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected abstract void UpdateItem(float deltaTime, T component);
+    }
+
+    /// <summary>
+    /// Represent a game system which supports two types.
+    /// </summary>
+    public abstract class GameSystem<T1, T2> : GameSystem
+        where T1 : IComponent
+        where T2 : IComponent
+    {
+        protected GameSystem() 
+            : base(typeof(T1), typeof(T2)) { }
+
+        public override void Update(float deltaTime)
+        {
+            PreUpdate(deltaTime);
+            foreach (var entity in Entities)
+            {
+                T1 component1 = entity.Get<T1>();
+                T2 component2 = entity.Get<T2>();
+
+                UpdateItem(deltaTime, component1, component2);
+            }
+        }
+
+        /// <summary>
+        /// Occur before update.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PreUpdate(float deltaTime) { }
+
+        /// <summary>
+        /// Occur after update.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PostUpdate(float deltaTime) { }
+
+        /// <summary>
+        /// Update item.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected abstract void UpdateItem(float deltaTime, T1 component1, T2 component2);
+    }
+
+    /// <summary>
+    /// Represent a game system which supports three types.
+    /// </summary>
+    public abstract class GameSystem<T1, T2, T3> : GameSystem
+        where T1 : IComponent
+        where T2 : IComponent
+    {
+        protected GameSystem()
+            : base(typeof(T1), typeof(T2), typeof(T3)) { }
+
+        public override void Update(float deltaTime)
+        {
+            PreUpdate(deltaTime);
+            foreach (var entity in Entities)
+            {
+                T1 component1 = entity.Get<T1>();
+                T2 component2 = entity.Get<T2>();
+                T3 component3 = entity.Get<T3>();
+
+                UpdateItem(deltaTime, component1, component2, component3);
+            }
+        }
+
+        /// <summary>
+        /// Occur before update.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PreUpdate(float deltaTime) { }
+
+        /// <summary>
+        /// Occur after update.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PostUpdate(float deltaTime) { }
+
+        /// <summary>
+        /// Update item.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected abstract void UpdateItem(float deltaTime, T1 component1, T2 component2,
+            T3 component3);
     }
 }
--- GameLib/Systems/Base/RenderSystem.cs
+++ GameLib/Systems/Base/RenderSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace GameLib
 {
     /// <summary>
@@ -15,5 +16,129 @@ namespace GameLib
         /// </summary>
         /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
         public abstract void Render(float deltaTime, IRenderer renderer);
+    }
+
+    /// <summary>
+    /// Represent a render system which supports one type.
+    /// </summary>
+    public abstract class RenderSystem<T> : RenderSystem
+        where T : IComponent
+    {
+        protected RenderSystem()
+            : base(typeof(T)) { }
+
+        public override void Render(float deltaTime, IRenderer renderer)
+        {
+            PreRender(deltaTime);
+            foreach (var entity in Entities)
+            {
+                T component = entity.Get<T>();
+
+                RenderItem(deltaTime, renderer, component);
+            }
+        }
+
+        /// <summary>
+        /// Occur before render.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PreRender(float deltaTime) { }
+
+        /// <summary>
+        /// Occur after render.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PostRender(float deltaTime) { }
+
+        /// <summary>
+        /// Render item.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected abstract void RenderItem(float deltaTime, IRenderer renderer, T component);
+    }
+
+    /// <summary>
+    /// Represent a render system which supports two types.
+    /// </summary>
+    public abstract class RenderSystem<T1, T2> : RenderSystem
+        where T1 : IComponent
+        where T2 : IComponent
+    {
+        protected RenderSystem()
+            : base(typeof(T1), typeof(T2)) { }
+
+        public override void Render(float deltaTime, IRenderer renderer)
+        {
+            PreRender(deltaTime);
+            foreach (var entity in Entities)
+            {
+                T1 component1 = entity.Get<T1>();
+                T2 component2 = entity.Get<T2>();
+
+                RenderItem(deltaTime, renderer, component1, component2);
+            }
+        }
+
+        /// <summary>
+        /// Occur before render.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PreRender(float deltaTime) { }
+
+        /// <summary>
+        /// Occur after render.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PostRender(float deltaTime) { }
+
+        /// <summary>
+        /// Render item.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected abstract void RenderItem(float deltaTime, IRenderer renderer,
+            T1 component1, T2 component2);
+    }
+
+    /// <summary>
+    /// Represent a render system which supports three types.
+    /// </summary>
+    public abstract class RenderSystem<T1, T2, T3> : RenderSystem
+        where T1 : IComponent
+        where T2 : IComponent
+    {
+        protected RenderSystem()
+            : base(typeof(T1), typeof(T2), typeof(T3)) { }
+
+        public override void Render(float deltaTime, IRenderer renderer)
+        {
+            PreRender(deltaTime);
+            foreach (var entity in Entities)
+            {
+                T1 component1 = entity.Get<T1>();
+                T2 component2 = entity.Get<T2>();
+                T3 component3 = entity.Get<T3>();
+
+                RenderItem(deltaTime, renderer, component1, component2, component3);
+            }
+        }
+
+        /// <summary>
+        /// Occur before render.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PreRender(float deltaTime) { }
+
+        /// <summary>
+        /// Occur after render.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected virtual void PostRender(float deltaTime) { }
+
+        /// <summary>
+        /// Render item.
+        /// </summary>
+        /// <param name="deltaTime">Time ellapsed between last and current frame.</param>
+        protected abstract void RenderItem(float deltaTime, IRenderer renderer,
+            T1 component1, T2 component2, T3 component3);
     }
 }
