9b2b391ebe46273470262c2123d4f928bccb0b05
Kotáb Petr <518-kotabpe@users.noreply.localhost>
2021-08-20 19:14:21 +0200

    Apperance component.

---
+++ GameLib/Components/Apperance.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+
+namespace GameLib.Components
+{
+    /// <summary>
+    /// Represent an apperance of an entitiy.
+    /// </summary>
+    public class Apperance : IComponent
+    {
+        public List<Sprite> Sprites { get; set; }
+    }
+}
