c343ac2df9a0d46e46c70367414be44c1f53c0c9
Kotáb Petr <518-kotabpe@users.noreply.localhost>
2021-08-20 19:13:10 +0200

    Added sprite.

---
+++ GameLib/Sprite.cs
@@ -0,0 +1,17 @@
+﻿namespace GameLib
+{
+    /// <summary>
+    /// Represent a sprite.
+    /// </summary>
+    public class Sprite
+    {
+        public string Name { get; private set; }
+
+        public Transform Transform { get; set; }
+
+        public Sprite(string name)
+        {
+            Name = name;
+        }
+    }
+}
