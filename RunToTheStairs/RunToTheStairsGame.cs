7233aec27e5266106be0d7945c0613a7845ac5b5
Kotáb Petr <518-kotabpe@users.noreply.localhost>
2021-08-21 16:36:34 +0200

    Project tweak.

---
+++ RunToTheStairs/RunToTheStairsGame.cs
@@ -0,0 +1,27 @@
+﻿using GameLib;
+using GameLib.Systems;
+
+using System.Collections.Generic;
+
+
+namespace RunToTheStairs
+{
+    public class RunToTheStairsGame : Game
+    {
+        protected override IEnumerable<GameSystem> InitializeGameSystems()
+        {
+            return new List<GameSystem>()
+            {
+
+            };
+        }
+
+        protected override IEnumerable<RenderSystem> InitializeRenderSystems()
+        {
+            return new List<RenderSystem>()
+            {
+                new SpriteRenderSystem(),
+            };
+        }
+    }
+}
