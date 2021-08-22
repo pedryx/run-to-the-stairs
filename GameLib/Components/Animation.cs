581476f9ef837557c277c3080f604855a77ef7ec
Kotáb Petr <518-kotabpe@users.noreply.localhost>
2021-08-22 15:03:21 +0200

    Sprite sheet animations.

---
+++ GameLib/Components/Animation.cs
@@ -0,0 +1,82 @@
+﻿using System.Numerics;
+using System.Xml.Serialization;
+
+
+namespace GameLib.Components
+{
+    /// <summary>
+    /// Represent a 2D animation for sprite sheets.
+    /// 
+    /// Animation is form the left to the right and on each line is different action.
+    /// </summary>
+    public class Animation : IComponent
+    {
+
+        /// <summary>
+        /// Size of the tile in sprite sheet.
+        /// </summary>
+        public Vector2 TileSize { get; set; } = new Vector2(64, 64);
+
+        /// <summary>
+        /// X coor of current tile.
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Y coor of current tile.
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Number of tiles in each row.
+        /// </summary>
+        public int Width { get; set; } = 9;
+
+        /// <summary>
+        /// Number of tiles in eahc column.
+        /// </summary>
+        public int Height { get; set; } = 4;
+
+        /// <summary>
+        /// X coor of first tile.
+        /// </summary>
+        public int StartX { get; set; } = 1;
+
+        /// <summary>
+        /// Y coor of first tile.
+        /// </summary>
+        public int StartY { get; set; } = 3;
+
+        /// <summary>
+        /// Number of frames per second.
+        /// </summary>
+        public float FPS { get; set; }
+
+        [XmlIgnore]
+        /// <summary>
+        /// Ellapsed time from last frame switch.
+        /// </summary>
+        public float EllapsedDelay { get; set; }
+
+        /// <summary>
+        /// Determine if animation is enabled.
+        /// </summary>
+        public bool Enabled { get; set; } = true;
+
+        public Animation()
+        {
+            X = StartX;
+            Y = StartY;
+            FPS = (Width - StartX);
+        }
+
+        public Rectangle GetClip() => new()
+        {
+            X = X * TileSize.X,
+            Y = Y * TileSize.Y,
+            Width = TileSize.X,
+            Height = TileSize.Y,
+        };
+
+    }
+}
---
+++ GameLib/Systems/AnimationSystem.cs
@@ -0,0 +1,35 @@
+﻿using GameLib.Components;
+
+
+namespace GameLib.Systems
+{
+    /// <summary>
+    /// Represent a system for 2D sprite sheets animation.
+    /// </summary>
+    public class AnimationSystem : GameSystem<Apperance, Animation>
+    {
+        protected override void UpdateItem(float deltaTime,
+            Apperance apperance, Animation animation)
+        {
+            if (animation.Enabled)
+            {
+                float delay = 1 / animation.FPS;
+
+                animation.EllapsedDelay += deltaTime;
+                if (animation.EllapsedDelay >= delay)
+                {
+                    animation.EllapsedDelay -= delay;
+                    animation.X++;
+
+                    if (animation.X == animation.Width)
+                        animation.X = animation.StartX;
+                }
+            }
+
+            foreach (var sprite in apperance.Sprites)
+            {
+                sprite.Clip = animation.GetClip();
+            }
+        }
+    }
+}
